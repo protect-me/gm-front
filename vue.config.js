afd26f9cecb2278d37fa07840b1e6d2085c377cf
protect-me <pmfwiww@gmail.com>
2021-07-26 18:58:08 +0900

    Feat: update vue.config.js for build directory

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: { //① api 요청이 있을때 어디에서 처리할지를 설정
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  outputDir: '../gm-back/public',  //② 배포 파일의 위치를 지정
 }
