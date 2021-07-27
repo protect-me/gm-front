d1b6ed2b8fe33e2b163ac3546c8f4d833a044704
protect-me <pmfwiww@gmail.com>
2021-07-27 13:16:50 +0900

    Feat: read exercise, set data on table

--- src/views/Exercise.vue
+++ src/views/Exercise.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="container">
-    <v-dialog v-model="dialog" persistent max-width="300px">
-      <!-- transition="dialog-bottom-transition" -->
+    <!-- transition="dialog-bottom-transition" -->
+    <!-- <v-dialog v-model="dialog" persistent max-width="300px">
       <template v-slot:activator="{ on, attrs }">
         <v-btn color="primary" dark v-bind="attrs" v-on="on">
           Open Dialog
         </v-btn>
       </template>
       <ExerciseForm @closeDialog="closeDialog"></ExerciseForm>
-    </v-dialog>
+    </v-dialog> -->
     <v-simple-table>
       <template v-slot:default>
         <thead>
           <tr>
             <th class="text-left">Name</th>
-            <th class="text-left">Calories</th>
+            <th class="text-left">Category</th>
+            <th class="text-left">Target</th>
+            <th class="text-left">Note</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in desserts" :key="item.name">
-            <td>{{ item.name }}</td>
-            <td>{{ item.calories }}</td>
+          <tr v-for="exercise in exercises" :key="exercise.id">
+            <td>{{ exercise.name }}</td>
+            <td>{{ exercise.category }}</td>
+            <td>{{ exercise.target }}</td>
+            <td>{{ exercise.note }}</td>
           </tr>
         </tbody>
       </template>
@@ -29,60 +33,21 @@
 </template>
 
 <script>
-import ExerciseForm from "@/components/ExerciseForm";
+// import ExerciseForm from "@/components/ExerciseForm";
 
 export default {
   components: {
-    ExerciseForm,
+    // ExerciseForm,
+  },
+  created() {
+    this.$http.get("/api/exercise").then((res) => {
+      this.exercises = res.data;
+    });
   },
   data() {
     return {
+      exercises: [],
       dialog: false,
-      notifications: false,
-      sound: true,
-      widgets: false,
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-        },
-      ],
     };
   },
   methods: {
