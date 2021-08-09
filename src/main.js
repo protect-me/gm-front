cfd3ac724d8d0ff97cea3c3ce963eae636858094
protect-me <pmfwiww@gmail.com>
2021-08-09 23:01:10 +0900

    Feat: show history, create historyCard, install momentjs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
+import './plugins/moment'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
