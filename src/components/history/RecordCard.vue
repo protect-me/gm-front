a963d0e8b3d5b7f40003dd111e461639ec26d7b3
protect-me <pmfwiww@gmail.com>
2021-08-15 18:40:08 +0900

    Fix: RecordCard startTime

--- src/components/history/RecordCard.vue
+++ src/components/history/RecordCard.vue
@@ -14,7 +14,7 @@
       </v-btn>
     </v-card-title>
     <v-card-subtitle class="pb-0">
-      {{ this.$moment(recordsGroup.startTime).format("YYYY.MM.DD (dd) HH:MM") }}
+      {{ this.$moment(recordsGroup.startTime).format("YYYY.MM.DD (dd) HH:mm") }}
     </v-card-subtitle>
     <v-card-subtitle class="pt-0 pb-0">
       {{ duration }}
